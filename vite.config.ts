d3e354194f8b297234128c7fe6c602adaebdb57b
Fikrisn <fikrisetiawan@polinema.ac.id>
2025-08-04 13:34:44 +0700

    yoi

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react()
-  ],
+  plugins: [react()],
+  base: '/', // Untuk username.github.io, gunakan '/'
   server: {
     port: 7482,
     open: true
   },
   build: {
-    outDir: 'build'
+    outDir: 'dist' // atau 'build'
   }
 })
